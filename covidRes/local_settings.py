b040f1ebb69e0a3563c0cf8da3daf731e408bb25
Jaime Rodriguez <jaime.rodriguez1295@gmail.com>
2020-03-23 13:42:24 +0000

    Use sqlite for local development and postgresqpl for production

--- covidRes/local_settings.py
+++ covidRes/local_settings.py
@@ -16,12 +16,8 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'local_db',
-        'USER': 'test',
-        'PASSWORD': 'test',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
