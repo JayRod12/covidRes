8810d9f28bfb97947f8409bdae08e80cd72f3400
Joan Palacios <jopabel93@gmail.com>
2020-03-28 12:50:26 +0100

    Machines tab included

---
+++ frontend/src/views/MachineList.js
@@ -0,0 +1,208 @@
+/*!
+
+=========================================================
+* Black Dashboard React v1.1.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/black-dashboard-react
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/black-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+
+// reactstrap components
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardTitle,
+  Table,
+  Row,
+  Col
+} from "reactstrap";
+
+class MachineRow extends React.Component {
+  render() {
+    return (
+          <React.Fragment>
+            <tr>
+              <td>{this.props.model_name}</td>
+              <td>{this.props.location}</td>
+              <td className="text-center">{this.props.patient_pk}</td>
+            </tr>
+          </React.Fragment>
+    );
+  }
+}
+function prettifyDate(raw_date) {
+  return new Date(raw_date).toGMTString();
+};
+
+class MachineList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      loaded: false,
+      placeholder: "Loading",
+      error_message: "",
+    };
+  }
+  componentDidMount() {
+    fetch("rest/machines/")
+            .then(response => {
+                if (response.status > 400) {
+                  throw new Error(response.status);
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+                this.setState(() => {
+                    return {
+                        data,
+                        loaded: true
+                    };
+                });
+            })
+            .catch(error => {
+              this.setState(() => {
+                return {
+                  loaded: true,
+                  placeholder: "Failed to load",
+                  error_message: "You don't have permission to view these machines.",
+                };
+              });
+            });
+  };
+  notify = place => {
+    var color = Math.floor(Math.random() * 5 + 1);
+    var type;
+    switch (color) {
+      case 1:
+        type = "primary";
+        break;
+      case 2:
+        type = "success";
+        break;
+      case 3:
+        type = "danger";
+        break;
+      case 4:
+        type = "warning";
+        break;
+      case 5:
+        type = "info";
+        break;
+      default:
+        break;
+    }
+    var options = {};
+    options = {
+      place: place,
+      message: (
+        <div>
+          <div>
+            Welcome to <b>Black Dashboard React</b> - a beautiful freebie for
+            every web developer.
+          </div>
+        </div>
+      ),
+      type: type,
+      icon: "tim-icons icon-bell-55",
+      autoDismiss: 7
+    };
+    this.refs.notificationAlert.notificationAlert(options);
+  };
+  render() {
+    if (!this.state.loaded) {
+      return (
+        <CardHeader>
+          <CardTitle tag="h4">Loading machines...</CardTitle>
+        </CardHeader>
+      );
+    }
+    let machines;
+    if (this.state.error_message.length > 0) {
+      machines = (
+        <Alert color="danger">
+          {this.state.error_message} Are you <a href="/admin" className="alert-link"> logged in?</a>
+        </Alert>
+      );
+    } else if (this.state.data.results.length > 0) {
+      machines = this.state.data.results.map((entry, index) => (
+        <MachineRow
+          key={entry.id}
+          model_name={entry.model_name}
+          location={entry.location}
+          patient_pk={entry.patient_pk}
+          />
+        )
+      );
+    } else {
+      machines = (
+        <CardText>No message history</CardText>
+      );
+    }
+    console.log(machines);
+    return (
+      <>
+        <div className="content">
+          <Row>
+            <Col md="12">
+              <Card>
+                <CardHeader>
+                  <CardTitle tag="h4">Machines</CardTitle>
+                </CardHeader>
+                <CardBody>
+                  <Table className="tablesorter" responsive>
+                    <thead className="text-primary">
+                      <tr>
+                        <th>Name</th>
+                        <th>Location</th>
+                        <th className="text-center">Patient</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {machines}
+                    </tbody>
+                  </Table>
+                </CardBody>
+              </Card>
+            </Col>
+            <Col md="12">
+              <Card className="card-plain">
+                <CardHeader>
+                  <CardTitle tag="h4">Machines on Plain Background</CardTitle>
+                </CardHeader>
+                <CardBody>
+                  <Table className="tablesorter" responsive>
+                    <thead className="text-primary">
+                      <tr>
+                        <th>Name</th>
+                        <th>Location</th>
+                        <th className="text-center">Patient</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {machines}
+                    </tbody>
+                  </Table>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+        </div>
+      </>
+    );
+  }
+}
+
+export default MachineList;
