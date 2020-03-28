cdab25b6b8d54871d3e219c40fca340ef30991f4
Mircea Gabriel Suciu <meercha@Mirceas-MacBook-Pro.local>
2020-03-28 16:04:47 +0000

    add NOW indnicator

--- frontend/src/views/Ventilators.js
+++ frontend/src/views/Ventilators.js
@@ -1,7 +1,14 @@
 import React from "react";
 import classNames from "classnames";
 
-import Timeline from 'src/react-calendar-timeline'
+import Timeline, {
+    TimelineHeaders,
+    SidebarHeader,
+    DateHeader,
+    TimelineMarkers,
+    TodayMarker,
+} from 'src/react-calendar-timeline'
+
 // make sure you include the timeline stylesheet or the timeline will not be styled
 import 'src/react-calendar-timeline/src/lib/Timeline.scss'
 import moment from 'moment'
@@ -15,36 +22,7 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 
-const groups = [{ id: 1, title: 'group 1' }, { id: 2, title: 'group 2' }]
-
-const ventilators = [
-    { id: 1, name: "Ventilator1" },
-    { id: 2, name: "Ventilator2" },
-    { id: 3, name: "Ventilator3" },
-    { id: 4, name: "Ventilator4" },
-    { id: 5, name: "Ventilator5" },
-    { id: 6, name: "Ventilator6" },
-    { id: 7, name: "Ventilator7" },
-    { id: 8, name: "Ventilator8" },
-    { id: 9, name: "Ventilator9" },
-    { id: 10, name: "Ventilator10" },
-];
-
-const assignments = [
-    { assignmentId: 1, ventilatorID: 1, patientName: "Dolores", location: "Room1", startDate: "2020-03-10", endDate: "2020-03-30" },
-    { assignmentId: 2, ventilatorID: 1, patientName: "Raph", location: "Room2", startDate: "2020-04-01", endDate: "2020-04-04" },
-    { assignmentId: 3, ventilatorID: 1, patientName: "Jaime", location: "Room2", startDate: "2020-04-10", endDate: "2020-04-22" },
-    { assignmentId: 4, ventilatorID: 2, patientName: "Ionut", location: "Room1", startDate: "2020-03-20", endDate: "2020-03-23" },
-    { assignmentId: 5, ventilatorID: 2, patientName: "Maria", location: "Room1", startDate: "2020-03-29", endDate: "2020-03-31" },
-    { assignmentId: 6, ventilatorID: 2, patientName: "Anca", location: "Room1", startDate: "2020-04-01", endDate: "2020-04-30" },
-    { assignmentId: 7, ventilatorID: 3, patientName: "Mircea", location: "Room3", startDate: "2020-03-01", endDate: "2020-03-20" },
-    { assignmentId: 8, ventilatorID: 4, patientName: "Joan", location: "Room3", startDate: "2020-03-14", endDate: "2020-03-27" },
-    { assignmentId: 9, ventilatorID: 4, patientName: "Patiente", location: "Room3", startDate: "2020-03-29", endDate: "2020-04-05" },
-    { assignmentId: 10, ventilatorID: 7, patientName: "Johnny", location: "Room1", startDate: "2020-03-01", endDate: "2020-03-30" },
-    { assignmentId: 11, ventilatorID: 8, patientName: "Ben", location: "Room5", startDate: "2020-03-20", endDate: "2020-03-22" },
-    { assignmentId: 12, ventilatorID: 8, patientName: "Josh", location: "Room5", startDate: "2020-03-24", endDate: "2020-03-29" },
-    { assignmentId: 13, ventilatorID: 10, patientName: "Chris", location: "Room5", startDate: "2020-03-01", endDate: "2020-04-01" },
-];
+const FIVE_SECONDS_MS = 5 * 1000;
 
 class Ventilators extends React.Component {
     constructor(props) {
@@ -214,7 +192,7 @@ class Ventilators extends React.Component {
                         <Timeline
                             groups={this.state.groups}
                             items={this.state.items}
-                            showCursorLine
+                            traditionalZoom
                             itemTouchSendsClick={true}
                             canMove={true}
                             canResize={"both"}
@@ -222,7 +200,24 @@ class Ventilators extends React.Component {
                             defaultTimeEnd={moment().add(12, 'day')}
                             onItemMove={this._handleItemMove}
                             onItemResize={this._handleItemResize}
-                        />
+                        >
+                            <TimelineHeaders className="sticky">
+                                <SidebarHeader>
+                                    {({ _ }) => {
+                                        return <div style={{ alignSelf: "center", color: "white", textAlign: "center", width: "150px" }}>Ventilators</div>;
+                                    }}
+                                </SidebarHeader>
+                                <DateHeader unit="primaryHeader" />
+                                <DateHeader />
+                            </TimelineHeaders>
+                            <TimelineMarkers>
+                                <TodayMarker interval={FIVE_SECONDS_MS}>
+                                    {({ styles, date }) =>
+                                        <div style={{ ...styles, backgroundColor: 'red' }} />
+                                    }
+                                </TodayMarker>
+                            </TimelineMarkers>
+                        </Timeline>
                     </Card>}
             </div>
         );
