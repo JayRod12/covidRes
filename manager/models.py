cea1aa62965934381579d0fa3c56305365c76c1c
Joan Palacios <jopabel93@gmail.com>
2020-04-01 11:59:35 +0200

    Patient's user saved as a field

--- manager/models.py
+++ manager/models.py
@@ -76,6 +76,7 @@ class Patient(models.Model):
             self.default_pass = '_'.join([random.choice("ant bee cow dog cat pet dot map set pig pen mat let wet".split(" ")) for n in range(4)])
             self.user = User.objects.create(username=self.name.split(" ")[0].replace("_", "").lower()+"_"+str(self.pk), password = self.default_pass, email="")
             self.user.save()
+            super(Patient, self).save(*args, **kwargs)
         if self.user.role is None:
             patient_role = Role.objects.filter(name="Patient")
             if patient_role:
