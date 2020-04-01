56532d443c78ebfb14c337b47cd985a96a01dfcf
Joan Palacios <jopabel93@gmail.com>
2020-04-01 23:44:06 +0200

    User information retrieve

--- manager/authentication.py
+++ manager/authentication.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import authenticate, login, logout
 from .models import User
+from django.http import HttpResponse
+import json
 
 def login_view(request):
     username = request.POST['username']
@@ -17,11 +19,29 @@ def logout_view(request):
 
 def password_view(request):
     username = request.POST['username']
-    password_old = request.POST['possword_old']
-    password = request.POST['possword']
+    password_old = request.POST['password_old']
+    password = request.POST['password']
     user = authenticate(request, username=username, password=password_old)
     if user is not None:
         user.set_password(password)
         return("Success")
     else:
         return("Wrong credentials")
+
+def user_view(request):
+    if request.user is not None:
+        user = request.user
+        if user.role is not None:
+            perm_str = "permission"
+            answer = vars(user.role)
+            answer = {key: answer[key] for key in answer if isinstance(key, str) and len(key) > len(perm_str) and key[:len(perm_str)] == perm_str}
+            answer["username"] = user.username
+            answer["role"] = user.role.name
+        else:
+            answer = {
+                "username": user.username,
+                "role": None
+            }
+        return HttpResponse(json.dumps(answer))
+    else:
+        return HttpResponse("Not logged in")
