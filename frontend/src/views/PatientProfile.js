4118af538ec16ab4a2572803db71157391898e0b
Joan Palacios <jopabel93@gmail.com>
2020-04-17 23:55:03 +0200

    Treatment plan made functional

--- frontend/src/views/PatientProfile.js
+++ frontend/src/views/PatientProfile.js
@@ -168,6 +168,20 @@ class PatientProfile extends React.Component {
         return response.json();
       }).then(data => {window.location.reload()})
   }
+  handlePlanChanges = (event) => {
+    event.preventDefault();
+    console.log("PLANCHANGE");
+    console.log(event);
+    console.log(event.target);
+    console.log(event.target.id);
+    var plan_array = this.state.plan_array
+    const index = event.target.id.slice(1)
+    var item = plan_array[index]
+    item[event.target.name.slice(5)] = event.target.value
+    console.log(item);
+    plan_array[index] = item
+    this.setState({plan_array: plan_array})
+  }
   handleSubmit(event) {
     event.preventDefault();
     const data = new FormData(event.target);
@@ -181,6 +195,7 @@ class PatientProfile extends React.Component {
         id1: data.get('ID1'),
         bool_connected: this.state.bool_connected
     }
+    console.log("PLANTEXT: ", body['treatment_plan']);
     if(data.get('birth').length > 0){
       body['birth'] = new Date(data.get('birth')).toISOString()
     }
@@ -542,22 +557,23 @@ class PatientProfile extends React.Component {
                           {this.state.plan_array.map((item, iter) => {return(
                             <tr>
                               <td>
-                                 <FormGroup>
-                                   <label>{t("Model")}</label>
-                                   <Input
-                                     defaultValue={item.model}
-                                     type="select"
-                                   >
-                                    {models}
-                                   </Input>
-                                 </FormGroup>
+                               <Input
+                                 id={'m' + iter}
+                                 defaultValue={item.model}
+                                 name="plan_model"
+                                 onChange={this.handlePlanChanges}
+                                 type="select"
+                               >
+                                {models}
+                               </Input>
                               </td>
                               <td>
                                 <TextField
-                                  id={iter}
-                                  label="Start"
+                                  id={'s' + iter}
                                   type="date"
                                   defaultValue={moment(item.start).format("YYYY-MM-DD")}
+                                  name="plan_start"
+                                  onChange={this.handlePlanChanges}
                                   InputLabelProps={{
                                     shrink: true,
                                   }}
@@ -565,10 +581,11 @@ class PatientProfile extends React.Component {
                               </td>
                               <td>
                                 <TextField
-                                  id={iter}
-                                  label="End"
+                                  id={'e' + iter}
                                   type="date"
-                                  defaultValue={moment(item.start).format("YYYY-MM-DD")}
+                                  defaultValue={moment(item.end).format("YYYY-MM-DD")}
+                                  name="plan_end"
+                                  onChange={this.handlePlanChanges}
                                   InputLabelProps={{
                                     shrink: true,
                                   }}
